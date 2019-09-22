f962a45e4846e6b4f370a6539b542a24df4f7b4b
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2019-09-22 13:45:32 -0400

    Adicionado in-view e adicionado scroll do in view na pagina de servico

--- src/assets/js/services/services.js
+++ src/assets/js/services/services.js
@@ -1,1 +1,12 @@
+import inView from "in-view";
+
 require('../menu');
+
+inView.threshold(0.5);
+inView('.service')
+    .on('enter', (el) => {
+        document.body.setAttribute(`class`, el.getAttribute(`data-section`));
+    })
+    .on('exit', (el) => {
+        // document.body.setAttribute(`class`, ``);
+    });
